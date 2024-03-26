77bf01b7dabd3d8ed48becd1511b7dce474aedd9
BohdanPrihodko <157399457+BohdanPrihodko@users.noreply.github.com>
2024-03-26 21:29:27 +0200

    fix js id

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[menu-modal-open]'),
-    closeModalBtn: document.querySelector('[menu-modal-close]'),
-    modal: document.querySelector('[menu-modal]'),
+    openModalBtn: document.querySelector('#menu-modal-open'),
+    closeModalBtn: document.querySelector('#menu-modal-close'),
+    modal: document.querySelector('#menu-modal'),
   };
   refs.openModalBtn.addEventListener('click', toggleModalMenu);
   refs.closeModalBtn.addEventListener('click', toggleModalMenu);
